7fc1cf5cc37de1359395da9fadf582477dbfec74
Martin Sh <martinshn@gmail.com>
2021-10-29 12:33:31 +0300

    deploy

---
+++ precache-manifest.fb0f738b07b6dc71676c05d87c7dd604.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1bd1c268953d3bd447",
+    "url": "/misterbitcoin/css/app.90fd91a9.css"
+  },
+  {
+    "revision": "c050351a9b63a7d08e565b29ee779936",
+    "url": "/misterbitcoin/fonts/fa-solid-900.c050351a.ttf"
+  },
+  {
+    "revision": "f01574120e1dfb35101e2a37bf084738",
+    "url": "/misterbitcoin/img/logo.f0157412.png"
+  },
+  {
+    "revision": "1c279a3874f22c1ef23ecc4e73a5055c",
+    "url": "/misterbitcoin/index.html"
+  },
+  {
+    "revision": "8c1bd1c268953d3bd447",
+    "url": "/misterbitcoin/js/app.70425985.js"
+  },
+  {
+    "revision": "80e1b2aabfb54279dd90",
+    "url": "/misterbitcoin/js/chunk-vendors.938b4bb5.js"
+  },
+  {
+    "revision": "f01574120e1dfb35101e2a37bf084738",
+    "url": "/misterbitcoin/logo.png"
+  },
+  {
+    "revision": "dfdd4a9d38e309edf021d8adc43a3596",
+    "url": "/misterbitcoin/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/misterbitcoin/robots.txt"
+  }
+]);
